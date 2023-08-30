e7d7c05fff89df1037fbaf933d598889c604e528
manish <manisharebsnl@gmail.com>
2023-08-30 22:02:25 +0530

    Day-1 solutions with notes

---
+++ Day-1/solution.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const filePath = 'input.txt';
+
+
+fs.readFile(filePath, 'utf-8', (err,data) => {
+	if(err) {
+		console.error(err);
+		return;
+	}
+    const depths = data.split('\n'); 
+
+    function part1(){
+        let count = 0;
+        for (let i=0; i < depths.length-1; i++){
+            if (parseInt(depths[i+1]) > parseInt(depths[i])){
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+    
+    //bruteforce
+    function part2() {
+        let count = 0;
+        let sum;
+        const subArraySum = []
+        for (let i = 0; i < depths.length-2; i++){
+            sum = 0;
+            sum = sum + parseInt(depths[i]);
+            for (let j = i + 1; j < i + 3; j++) {
+                sum = sum + parseInt(depths[j]);
+            }
+            subArraySum.push(sum);
+        }
+        for (let i=0; i < subArraySum.length-1; i++){
+            if (parseInt(subArraySum[i+1]) > parseInt(subArraySum[i])){
+                count = count + 1;
+            }
+        }
+        return count;
+        
+    }
+
+    console.log(part1());
+    console.log(part2());
+
+});
