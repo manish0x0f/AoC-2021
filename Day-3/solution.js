e8e821d15ddd614d036c1c9cad70d2ba1345ae72
manish0x0f <manish0x0f@gmail.com>
2023-09-05 23:01:21 +0530

    Day-3 part1 added

--- Day-3/solution.js
+++ Day-3/solution.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { cpuUsage } = require('process');
 
 fs.readFile('input.txt', 'utf-8', (err, data) => {
     if(err){
@@ -7,20 +8,38 @@ fs.readFile('input.txt', 'utf-8', (err, data) => {
     }
 
     const bDigits = data.split('\n');
-    console.log(bDigits);
 
     const bDigitsTranspose = [];
 
     for(let i = 0; i < bDigits.length; i++) {
-        console.log(bDigits[i]);
         for (let j = 0; j < bDigits[i].length; j++) {
-            console.log(bDigits[i][j]);
             bDigitsTranspose[j] = bDigitsTranspose[j] === undefined ? bDigits[i][j] : bDigitsTranspose[j] + bDigits[i][j];
         }
         
     }
+    
+    function part1()
+    {
+        const gammaRate = bDigitsTranspose.map(x => x.split('')).map(x => x.sort()).reduce((acc,curr) => {
+            acc += curr[Math.floor(curr.length/2)];
+            return acc;
+        },"");
 
-    console.log(bDigitsTranspose);
+        const epsilonRate = gammaRate.split('').map(x => x === '0' ? '1' : '0' ).join('');
+
+        return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+
+    }
+
+    console.log(part1());
+    
+    
+
+    
+    
+    
+
+    
 
     
 
